3f73dafe9faf444a69dbacf003710152b98bfa11
Teodor Todorov <teodortodorov752@gmail.com>
2024-08-02 23:33:19 +0300

    did small changes to host on vercel

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ app.post('/logs', (req, res) => {
 });
 
 // Server
-const PORT = 5000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
