8558b745671b1f0bdf449a5a4084a93b37810b5f
Teodor Todorov <teodortodorov752@gmail.com>
2024-08-02 23:56:16 +0300

    removed app.listen, fixed vercel locally

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-
+const path = require('path');
 const app = express();
-
-
 
 // Middleware to handle JSON data
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: 'hbs',
+    })
+);
 app.set('view engine', 'hbs');
 
 // Routes
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.post('/logs', (req, res) => {
     const logs = req.body.logs;
-    res.render('partials/logs', {logs});
+    res.render('partials/logs', { logs });
 });
 
-// Server
-const PORT = process.env.PORT || 3030;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// // Server
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
