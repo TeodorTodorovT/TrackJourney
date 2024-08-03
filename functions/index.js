4af87ffe391e9393bafda5a01f4c7ec8957d8b3a
Teodor Todorov <teodortodorov752@gmail.com>
2024-08-03 16:28:03 +0300

    fix for netlify deploy

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+const serverless = require('serverless-http');
 
 const app = express();
 
@@ -33,3 +34,5 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+
+module.exports.handler = serverless(app);
