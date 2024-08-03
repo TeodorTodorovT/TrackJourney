90bb0a85d25ba04838063504a807279b4b1b33c1
Teodor Todorov <teodortodorov752@gmail.com>
2024-08-03 17:03:59 +0300

    fixed deploy issue

--- functions/index.mjs
+++ functions/index.mjs
@@ -4,8 +4,8 @@ import serverless from 'serverless-http';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
 const app = express();
@@ -13,7 +13,7 @@ const app = express();
 // Middleware to handle JSON data
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.engine(
     'hbs',
@@ -22,7 +22,7 @@ app.engine(
     })
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join('views'));
 
 // Routes
 
@@ -36,9 +36,9 @@ app.post('/logs', (req, res) => {
 });
 
 // Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 export const handler = serverless(app);
